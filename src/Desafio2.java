92591ba1a3e71f7b9206c370e5b7734b6fa6cfdb
VoyDcode <Victor_r.lima@hotmail.com>
2026-05-11 21:01:54 -0300

    primeiro commit

--- src/Desafio2.java
+++ src/Desafio2.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Desafio2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int numero;
+        int zerosSeguidos = 3;
+        int contadorPositivo = 0;
+        int contadorNegativo = 0;
+
+        while (true) {
+            System.out.println("Digite um numero: ");
+            numero = leitor.nextInt();
+
+            if (numero == 0) {
+                zerosSeguidos++;
+
+                if (zerosSeguidos == 4) {
+                    System.out.println("Você digitou 4 zeros. Programa encerrado.");
+                    break;
+                }
+            } else {
+                zerosSeguidos = 0;
+
+                if (numero > 0) {
+                    contadorPositivo++;
+                    System.out.println("O numero é positivo!");
+                } else {
+                    contadorNegativo++;
+                    System.out.println("O numero é negativo!");
+                }
+            }
+        }
+
+        System.out.println("Quantidade de positivos: " + contadorPositivo);
+        System.out.println("Quantidade de negativos: " + contadorNegativo);
+
+        leitor.close();
+    }
 }
--- src/Leitura.java
+++ src/Leitura.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+import java.util.function.DoubleToIntFunction;
 
 public class Leitura {
+    static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("Digite sua avaliação sobre o filme: ");
+            nota = entrada.nextDouble();
+            mediaAvaliacao += nota;
+        }
+        System.out.println("Média de avalição: " + mediaAvaliacao / 3);
+
+
+
+    }
 }
